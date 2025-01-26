7d49169d722226b01f166ba3176d5971106246fc
lpsggc <gabrielcamargolopess@gmail.com>
2025-01-26 14:10:34 +0000

    Criei o código conforme desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,36 @@
 //Teste larissa
 
 int main() {
+
+    int pturisticos; //pontos turísticos
+    char codcidade[15];  //código da cidade
+    char nome[15];
+    float area;
+    int pib;
+    int populacao;
+    float expectativida;   //expectativa de vida
+
+    printf("\nEntre com o código da cidade: ");
+    scanf("%s", &codcidade);
+    printf("\nQual o nome dessa cidade: ");
+    scanf("%s", &nome);
+    printf("\nQuantos pontos turísticos tem essa cidade? ");
+    scanf("%d", &pturisticos);
+    printf("\nQual a área dessa cidade? ");
+    scanf("%f", &area);
+    printf("\nQual é o PIB dessa cidade? ");
+    scanf("%d", &pib);
+    printf("\nQual a população dessa cidade? ");
+    scanf("%d", &populacao);
+    printf("\nQual a expectativa de vida dessa cidade? ");
+    scanf("%f", &expectativida);
+
+
+    printf("Obrigado!\n");
+    printf("Código da sua cidade: %s\n Nome dessa cidade: %s\n Pontos turísticos nessa cidade: %d\n Área da cidade: %f\n PIB da cidade: %d\n População dessa cidade: %d\n Expectativa de vida nessa cidade: %f",  codcidade, nome, pturisticos, area, pib, populacao, expectativida);
+    
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
